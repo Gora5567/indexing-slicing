154c42fcf3e05dd68bf203b8e950218ce25b7b27
egormarik <egormarikpt@gmail.com>
2024-10-06 12:10:50 +0300

    edited helper.py edited indexing-slicing-hw.py

--- indexing-slicing-hw.py
+++ indexing-slicing-hw.py
@@ -1,9 +1,26 @@
-print('Hello Python!'[2])
+import helper
+
+helper.start_task(1)
 
 print('Hello Python!'[2])
+
+helper.end_task(1)
+
+helper.start_task(2)
+
+print('Hello Python!'[2])
+
+helper.end_task(2)
+
+helper.start_task(3)
 
 print('Hello Python!'[0:2])
 print('Hello Python!'[:2])
 
-new_string = 'Hello Python!'[0:2] + 'th'
-print(new_string)
+helper.end_task(3)
+
+helper.start_task(4)
+
+print('Hello Python!'[6:7] + 'at' + 'h')
+
+helper.end_task(4)
